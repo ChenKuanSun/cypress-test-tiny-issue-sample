2e73370f7f9e630c857333ef686fcabe0a4a0e7d
CK Sun <fgriasa123@gmail.com>
2023-05-08 21:38:24 -0700

    feat(test): finish flaky testing file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,47 +1,64 @@
 /// <reference types="cypress" />
-describe("page", () => {
+
+/**
+ * Before each test, we are mocking all the API call and change the response.
+ * But if there is a api call in the background like something service,
+ * the intercept will fail due to Cypress's netStubbingState reset.
+ *
+ * Note: we are enabling the testIsolation to keep context between tests.
+ */
+describe("Situations where intercept fails in our app", () => {
   const setup = () => {
+    // Initial Cypress Commands takes sometime.
+    cy.wait(3000);
+    // Intercept the API call
     cy.intercept(
       {
-        method: "*",
-        url: "*",
-      },
-      (req) => {
-        req.reply((res) => {
-          res.body = {};
-        });
-      }
-    );
-    cy.intercept(
-      {
+        pathname: "/",
         method: "GET",
-        url: "*",
       },
       (req) => {
-        req.reply((res) => {
-          res.body = "intercepted api";
+        req.reply({
+          statusCode: 200,
+          body: JSON.stringify("intercepted api"),
         });
       }
     ).as("getData");
   };
 
   it("visits the app", () => {
+    // Visit the app
     cy.visit("https://main.d260hbq9idig9k.amplifyapp.com/");
 
+    // Check if the app is loaded
     cy.get("#welcome h1").should("contain", "Welcome cypress-sample-page");
   });
 
   it("intercepts and changes the response", () => {
+    // Setup the test
     setup();
-    cy.get("#call-api").click();
 
+    // Click the button to call the API
+    cy.get("#call-api-delay-1-second").click();
+
+    // Wait for the API call to complete
+    cy.wait(["@getData"]);
+
+    // Check if the response is intercepted
     cy.get("#response pre").should("contain", "intercepted api");
-    cy.get("#call-api").click();
+
+    // Call the API again but we don't wait for the response
+    cy.get("#call-api-delay-1-second").click();
+
+    // Then end the test immediately
   });
 
   it("intercept failed", () => {
+    // Setup the test
     setup();
-    cy.get("#call-api").click();
-    cy.wait(5000);
+    // Set wait time to 3 seconds make sure the all API call is completed
+    cy.wait(3000);
+
+    cy.get("#response pre").should("not.contain", "Hello from server!");
   });
 });
